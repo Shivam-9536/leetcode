0eaeaf49aa45a82c15b4b3158930cdcb675d89d1
shivam kumar upadhyay <shiv1998am@gmail.com>
2026-01-18 13:42:20 +0530

    Create 2288-CountOperationsToObtainZero

---
+++ 2288-CountOperationsToObtainZero/2288-CountOperationsToObtainZero.java
@@ -0,0 +1,15 @@
+// Last updated: 1/18/2026, 1:42:22 PM
+class Solution {
+    public int countOperations(int num1, int num2) {
+        int c=0;
+        while(num1!=0&&num2!=0){
+            if(num1>num2)
+            num1=num1-num2;
+            
+            else
+            num2=num2-num1;
+            c++;
+        }
+        return c;
+    }
+}
