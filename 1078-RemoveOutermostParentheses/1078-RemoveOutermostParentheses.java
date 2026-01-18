de3a96f2ac30e3abbe25f9b2f5662c7f3504061f
shivam kumar upadhyay <shiv1998am@gmail.com>
2026-01-18 13:43:47 +0530

    Create 1078-RemoveOutermostParentheses

---
+++ 1078-RemoveOutermostParentheses/1078-RemoveOutermostParentheses.java
@@ -0,0 +1,20 @@
+// Last updated: 1/18/2026, 1:43:48 PM
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder ans=new StringBuilder();
+        int d=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='('){
+                if(d>0)
+                ans.append(ch);
+                d++;
+            }
+            else{
+                d--;
+                if(d>0) 
+                ans.append(ch);
+            }
+        }
+        return ans.toString();
+    }
+}
