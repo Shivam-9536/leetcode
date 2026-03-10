663ce4269983c0e2749be0ae96f34c0a4a855bba
shivam kumar upadhyay <shiv1998am@gmail.com>
2026-03-10 11:56:54 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 10/03/2026, 11:56:53
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
