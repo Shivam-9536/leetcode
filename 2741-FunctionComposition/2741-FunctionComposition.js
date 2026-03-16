926f41efc3bf85d1ff7f1323c3310ba1ad9d1f50
shivam kumar upadhyay <shiv1998am@gmail.com>
2026-03-16 23:13:12 +0530

    Create 2741-FunctionComposition

---
+++ 2741-FunctionComposition/2741-FunctionComposition.js
@@ -0,0 +1,17 @@
+// Last updated: 16/03/2026, 23:13:10
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+   return function(x) {
+    for(let i=functions.length-1;i>=0;i--)
+      x = functions[i](x);
+    return x;
+  }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
