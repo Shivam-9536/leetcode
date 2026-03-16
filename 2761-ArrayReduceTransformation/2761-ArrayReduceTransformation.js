daae944419759b6df3c56dda4caccad67b8d5e95
shivam kumar upadhyay <shiv1998am@gmail.com>
2026-03-16 23:13:07 +0530

    Create 2761-ArrayReduceTransformation

---
+++ 2761-ArrayReduceTransformation/2761-ArrayReduceTransformation.js
@@ -0,0 +1,13 @@
+// Last updated: 16/03/2026, 23:13:06
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let val = init;
+  for (let i = 0; i < nums.length; i++)
+    val = fn(val, nums[i]);
+  return val;
+};
