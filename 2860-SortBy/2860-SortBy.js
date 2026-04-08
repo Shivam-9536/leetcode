e5b685d611af216a754e033a229886e7ac53c4a2
shivam kumar upadhyay <shiv1998am@gmail.com>
2026-04-08 14:37:15 +0530

    Create 2860-SortBy

---
+++ 2860-SortBy/2860-SortBy.js
@@ -0,0 +1,9 @@
+// Last updated: 08/04/2026, 14:37:12
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+    return arr.sort((a, b) => fn(a) - fn(b));
+};
