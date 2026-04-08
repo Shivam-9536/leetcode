22b3d61e0c35a673c346b7a5201c669c84b39efb
shivam kumar upadhyay <shiv1998am@gmail.com>
2026-04-08 14:37:26 +0530

    Create 2798-ChunkArray

---
+++ 2798-ChunkArray/2798-ChunkArray.js
@@ -0,0 +1,12 @@
+// Last updated: 08/04/2026, 14:37:23
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    let res = [];
+  for (let i = 0; i < arr.length; i += size)
+    res.push(arr.slice(i, i + size));
+  return res;
+};
