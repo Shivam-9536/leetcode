72cbd384836497ce4da2ddd87d40c6fb7afc2e7c
shivam kumar upadhyay <shiv1998am@gmail.com>
2026-04-08 14:37:27 +0530

    Create 2796-AllowOneFunctionCall

---
+++ 2796-AllowOneFunctionCall/2796-AllowOneFunctionCall.js
@@ -0,0 +1,22 @@
+// Last updated: 08/04/2026, 14:37:25
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+   let called = false;
+  return function(...args) {
+    if (!called) {
+      called = true;
+      return fn(...args);
+    }
+  };
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
